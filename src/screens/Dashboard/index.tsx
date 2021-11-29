cf1678a54ad5ae598211de55534d446dcba5e5df
Bruno Fialho <brugerf@gmail.com>
2021-11-29 10:19:59 -0300

    feat: props with styled components

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -33,9 +33,24 @@ export function Dashboard() {
       </Header>
 
       <HightLightCards >
-        <HightLightCard />
-        <HightLightCard />
-        <HightLightCard />
+        <HightLightCard
+          type="up"
+          title="Entradas" 
+          amount="R$ 17.400,00" 
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HightLightCard
+          type="down"
+          title="Saídas" 
+          amount="R$ 1.259,00" 
+          lastTransaction="Última saída dia 03 de abril"
+        />
+        <HightLightCard
+          type="total"
+          title="Total" 
+          amount="R$ 16.141,00" 
+          lastTransaction="01 a 16 de abril"
+        />
       </HightLightCards>
     </Container>
   );
