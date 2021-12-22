b4ec5c0a011b25bcf25c9f8e2ddeb1cdb6277a89
Bruno Fialho <brugerf@gmail.com>
2021-12-21 23:04:14 -0300

    add: useFocusEffect

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { HighLightCard } from '../../components/HighlightCard';
@@ -20,6 +20,7 @@ import {
   TransactionList,
   LogoutButton
  } from './styles';
+import { useFocusEffect } from '@react-navigation/native';
 
  export interface DataListProps extends TransactionCardProps {
    id: string;
@@ -63,6 +64,12 @@ export function Dashboard() {
   useEffect(() => {
     loadTransactions();
   }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      loadTransactions();
+    }, [])
+  );
 
   return (
     <Container>
